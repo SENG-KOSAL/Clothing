eaf0771aa215ecc67dac488593ac85dba64fb0aa
SENG-KOSAL <sengkosal023@gmail.com>
2024-10-11 16:29:36 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './routers'; // Correct path to the router file
+
+
+createApp(App)
+  .use(router)
+  .mount('#app');
